3b43c0a633b908506dd616114d40378e18cd0029
Yi Ren <coderquestny@gmail.com>
2015-05-05 18:59:34 -0400

    editing signup.erb page"

--- models.rb
+++ models.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
 	has_many :posts
-	has_many :active_relationships, class_name: "Relationship", foreign_key: :follower_id, dependent: :destroy
-	has_many :passive_relationships, class_name: "Relationship", foreign_key: :followed_id, dependent: :destroy
+	# has_many :active_relationships, class_name: "Relationship", foreign_key: :follower_id, dependent: :destroy
+	# has_many :passive_relationships, class_name: "Relationship", foreign_key: :followed_id, dependent: :destroy
 end
 
 class Post < ActiveRecord::Base
@@ -11,5 +11,5 @@ end
 class Relationship < ActiveRecord::Base
 	belongs_to :follower, class_name: "User"
 	belongs_to :followed, class_name: "User"
-	validate_uniqueness_of :follower_id, scope: :followed_id
+	# validate_uniqueness_of :follower_id, scope: :followed_id
 end
